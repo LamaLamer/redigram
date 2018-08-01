3bc474e532c4c05c2bc3f0b10e891a4a3d78be06
Ilia Choly <icholy@accipiterradar.com>
2018-08-01 10:57:08 -0400

    remove intermediate image

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"github.com/ahmdrz/goinsta"
@@ -8,7 +9,6 @@ import (
 	"image/jpeg"
 	"log"
 	"math/rand"
-	"os"
 	"sort"
 	"time"
 )
@@ -51,35 +51,22 @@ func DoPost() error {
 			return err
 		}
 		fmt.Printf("%d: %s\n", s.Score, s.Title)
-		if err := SaveJPEG(im, "out.jpeg"); err != nil {
-			return err
-		}
-		return PostImage("out.jpeg")
+		return PostImage(im)
 	}
 	return fmt.Errorf("all %d submissions are used", len(ss))
 }
 
-func SaveJPEG(m image.Image, filename string) error {
-	f, err := os.Create(filename)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	return jpeg.Encode(f, m, &jpeg.Options{jpeg.DefaultQuality})
-}
-
-func PostImage(imgpath string) error {
+func PostImage(m image.Image) error {
 	insta := goinsta.New(*username, *password)
 	if err := insta.Login(); err != nil {
 		return fmt.Errorf("failed to login: %s", err)
 	}
 	defer insta.Logout()
-	f, err := os.Open(imgpath)
-	if err != nil {
+	var buf bytes.Buffer
+	if err := jpeg.Encode(&buf, m, nil); err != nil {
 		return err
 	}
-	defer f.Close()
-	if _, err := insta.UploadPhoto(f, *caption, 87, 0); err != nil {
+	if _, err := insta.UploadPhoto(&buf, *caption, 87, 0); err != nil {
 		return fmt.Errorf("failed to upload:", err)
 	}
 	return nil
